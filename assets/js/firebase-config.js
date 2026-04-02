ab7cb49b5b51d9bac37e445238dd2ddb6296c9af
asbel4096-web <asbel4096@gmail.com>
2026-04-02 07:08:11 +0200

    Add files via upload

---
+++ assets/js/firebase-config.js
@@ -0,0 +1,9 @@
+// ضع إعدادات Firebase هنا عند التفعيل الحقيقي
+window.firebaseSettings = {
+  apiKey: "PUT_YOUR_KEY",
+  authDomain: "PUT_YOUR_DOMAIN",
+  projectId: "PUT_YOUR_PROJECT_ID",
+  storageBucket: "PUT_YOUR_BUCKET",
+  messagingSenderId: "PUT_YOUR_SENDER_ID",
+  appId: "PUT_YOUR_APP_ID"
+};
